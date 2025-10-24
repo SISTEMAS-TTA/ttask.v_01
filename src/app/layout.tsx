cd864d1ec9779c1910a3246f9607b0f9ef8792e5
SISTEMAS-TTA <sistemas@ttarquitectos.mx>
2025-10-24 11:09:58 -0600

    feat: icono de tt listo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,11 @@ import NavigationGuard from "@/components/NavigationGuard";
 export const metadata: Metadata = {
   title: "ERP",
   description: "Sistema de gestión de indicaciones empresariales",
+  icons: [
+    { rel: "icon", url: "/icono_tt.svg", sizes: "32x32", type: "image/png" },
+    { rel: "apple-touch-icon", url: "/icono_tt.svg" },
+  ],
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
@@ -14,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        <meta name="theme-color" content="#ffff"/>  
+      </head>
       <body className="min-h-screen bg-gray-50">
         <NavigationGuard />
         {children}
